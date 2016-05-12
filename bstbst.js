e2a679e39602709456f3e005812919dcfcc63b6d
danFbach <quiktekrepair@gmail.com>
2016-05-12 17:12:30 -0500

    update to binary search, first node entry is root, filters out all chars, except for numbers.

--- bstbst.js
+++ bstbst.js
@@ -5,15 +5,7 @@ const getData = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-
-var node = new Node(25,null,null);
-node.newData(15);
-node.newData(65);
-node.newData(35);
-node.newData(5);
-node.newData(85);
-node.newData(45);
-node.newData(10);
+var node = new Node(null,null,null);
 var newInput = new UserInput();
 newInput.bstOptions();
 
@@ -23,24 +15,26 @@ function Node(value, left, right){
 	this.right = right;
 
 	this.newData = function(value){
+		if(this.value === null){			
+			node = new Node(value,null,null);
+		}
 		if(value === this.value){
-				if(newInput){
-					newInput.bstOptions();					
-				}}
+			if(newInput){
+				newInput.bstOptions();
+			}
+		}
 		else if(value < this.value)
 		{
 			if(this.left)
 			{
-				this.left.newData(value);				
+				this.left.newData(value);
 				return;
 			}
 			else
 			{
 				this.left = new Node(value, null, null);
-				if(newInput){
-					console.log("You've added",value,"as a new node.")
-					newInput.bstOptions();					
-				}
+				console.log("You've added",value,"as a new node.")
+				newInput.bstOptions();
 			}
 		}
 		else if(value > this.value)
@@ -53,15 +47,13 @@ function Node(value, left, right){
 			else
 			{
 				this.right = new Node(value, null, null);
-				if(newInput){
-					console.log("You've added",value,"as a new node.")
-					newInput.bstOptions();					
-				}
+				console.log("You've added",value,"as a new node.");
+				newInput.bstOptions();
 			}
 		}
 		else{
-			newInput.bstOptions();		
-			}		
+			newInput.bstOptions();
+			}
 	};
 	this.displayNodePath = function(value, directions){
 		if(value === this.value){
@@ -72,25 +64,25 @@ function Node(value, left, right){
 			if(this.left){
 				var leftWord = 'left';
 				directions = directions.concat(' ',this.value,', ',leftWord);
-				this.left.displayNodePath(value, directions);	
+				this.left.displayNodePath(value, directions);
 			}else{
-				console.log('The node,', value,'does not exist.')
+				console.log('The node,', value,'does not exist.');
 				newInput.bstOptions();
 			}
 		}else if(value > this.value){
 			if(this.right){
 				var rightWord = 'right';
 				directions = directions.concat(' ',this.value,', ',rightWord)
-				this.right.displayNodePath(value, directions);				
+				this.right.displayNodePath(value, directions);
 			}else{
-				console.log('The node,', value,'does not exist.')
+				console.log('The node,', value,'does not exist.');
 				newInput.bstOptions();
 			}
 		}else{
-			console.log('undefined')
+			console.log('undefined');
 			newInput.bstOptions();
 		}
-	};	
+	};
 };
 
 function UserInput(){
@@ -102,22 +94,30 @@ function UserInput(){
 			}
 			else if(answer === 2){
 				newInput.searchNode();
-			}  
+			}
 			else if(answer === 3){
 				console.log(node);
-				newInput.bstOptions();		
+				newInput.bstOptions();
 			}
 			else if(answer === 4){
-				getData.close();		
+				getData.close();
 			}
-			else{newInput.bstOptions();}				
+			else{
+				newInput.bstOptions();
+			}
 		});
 	};
-	
+
 	this.addNode = function(){
 		getData.question('Please enter a number for a new node. ', function(answer){
 			answer = Number(answer);
-			node.newData(answer);
+			if(0 <= answer || 0 > answer){
+				node.newData(answer);
+			}
+			else{
+				console.log('Invalid Entry.');
+				newInput.bstOptions();
+			}
 		});
 	};
 	this.searchNode = function(){
